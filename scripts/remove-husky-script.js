b247cf2ad1d689c8f5909f7b4c06b65b75b07533
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-07-13 10:47:32 +0200

    chore: shebang fix added

--- scripts/remove-husky-script.js
+++ scripts/remove-husky-script.js
@@ -3,5 +3,5 @@ const path = require('path');
 const pkgPath = path.resolve(__dirname, '../package.json');
 const pkg = require(pkgPath);
 
-delete pkg.scripts.postinstall; // or .prepare
+delete pkg.scripts.prepare; // or .prepare
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
